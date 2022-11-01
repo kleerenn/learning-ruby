d4517466b5eddb6d1d6a35d9c11080560ab19769
karen lee <hjlee8820@gmail.com>
2022-11-01 18:45:17 -0500

    add oop to openbook

--- projects/KL/bank.rb
+++ projects/KL/bank.rb
@@ -9,93 +9,103 @@ Primary user can ONLY see who has the most and least money
 Primary user can delete relatives from the account
 =end
 
+module Utilities
+    def validate_balance(balance)
+        valid = true
+        if balance.to_i < 0 || balance.is_a?(String) 
+            puts "it's less than 0 or string!!!!!"
+            valid = false
+        end
+    end
 
-# Variables
-$users = [1111]
-$is_exisiting = false
-$ssn
-$accounts = []
-$account_id = 0
+    def validate_ssn(ssn)
+        puts ssn
+        valid = true
+        if ssn.to_s != 4 || ssn.is_a?(String)
+            puts "wrong ssn"
+            valid = false
+        end
+    end
 
-
-# Methods
-def show_start_message
-    puts "Hello, please enter the last 4 digits of your social security number\n
-        >> Numbers: "
 end
 
-def check_exising_user(number)
-    $is_exisiting = $users.include?(number)
-end
+module Prompt
+    def show_start_message
+        puts "Hello, Would you like to create an account? (Y/n)"
+    end
 
-def show_options_for_exisiting_user
-    puts "Welcome back, what would you like to do? Choose an options: \n
-        1. View my account
-        2. Add deposit
-        3. Transfer Money
-        4. Withdrawl mondy
-        5. Add users to my account"
-end
+    def get_new_account_info
+        puts "Enter your name: "
+        name = gets.chomp
 
-def show_optopns_for_new_user
-    puts "User is not found. Would you like to create an account? Y / N"
-end
+        puts "Enter amount of money for initial deposit:"
+        balance = gets.chomp
 
-def view_account
-end
+        puts "Enter the last 4 digit of SSN:"
+        ssn = gets.chomp
 
-def add_deposit
-end
-
-def transfer_money
-end
-
-def withrawl_money
-end
-
-def add_users
-end
-
-def create_account(name, deposit)
-    puts "will create account #{name}, #{deposit}, #{$ssn}"
-    $accounts.push({
-        id: $account_id,
-        name: name,
-        deposit: deposit,
-        ssn: $ssn,
-    })
-    $account_id += 1
-    puts $accounts
-end
-
-def show_menu_for_existing_user(option)
-    case option
-        when '1'
-            puts "1 "
-        when '2'
-            puts "2"
-        when '3'
-            puts "3"
-        when '4'
-            puts "4"
-        when '5'
-            puts "5"
-        else 
-            puts " byy"
+        return { name: name, balance: balance, ssn: ssn }
     end
 end
 
-def show_menu_for_new_user(option)
+
+
+include Utilities
+include Prompt
+
+$accounts = []
+class Account
+    @@id = 0
+
+    def initialize(name, balance, ssn)
+        @name = name
+        @balance = balance
+        @ssn = ssn
+        @@id += 1
+    end
+
+    def view_account
+    end
+
+    def add_balance
+    end
+
+    def transfer_money
+    end
+
+    def withrawl_money
+    end
+
+    def add_users
+    end
+
+    def create_account
+    end
+
+end
+
+def create_account(name, balance, ssn)
+    puts "Your input is #{name}, #{balance}, #{ssn}"
+
+    valid_balance = Utilities.validate_balance(balance)
+    valid_ssn = Utilities.validate_ssn(ssn)
+   
+    if !valid_balance || !valid_ssn
+        puts "Invalid input - can't create an account."
+    end
+
+    account = Account.new(name, balance, ssn)
+    # $account.push(account)
+end
+
+
+def execute_option(option)
+    
     case option
         when 'y'
-            puts "Enter your name: "
-            name = gets.chomp
-            puts "Enter amount of money for initial deposit"
-            deposit = gets.chomp
-            if deposit < 0
-                puts "Deposit can't be lower than 0. Please enter again: "
-            end
-            create_account(name, deposit)    
+            info = Prompt.get_new_account_info
+            puts info
+            create_account(info[:name],info[:balance], info[:ssn])    
         else 
             puts "Not a valid answer"
     end
@@ -106,24 +116,6 @@ end
 
 #####
 
-show_start_message
-$ssn = gets.chomp.to_i
-
-check_exising_user($ssn)
-
-if $is_exisiting
-    show_options_for_exisiting_user
-else 
-   show_optopns_for_new_user
-end
-
-option = gets.chomp
-
-
-
-if $is_exisiting
-    show_menu_for_existing_user(option)
-else
-    show_menu_for_new_user(option.downcase)
-end
-
+Prompt.show_start_message
+option = gets.chomp.downcase
+execute_option(option)
